186a6244ee0f4aa3db5f9824e56c30a1538a59a3
mdmubin <mdmubin.xyz@gmail.com>
2024-09-27 22:41:07 +0600

    fix default type for `enable_if_t`.

--- rexbase/inc/rex/traits/enable_if.hpp
+++ rexbase/inc/rex/traits/enable_if.hpp
@@ -6,6 +6,6 @@ template <bool, typename t = void> struct enable_if          {};
 template       <typename t>        struct enable_if<true, t> { using type = t; };
 
 /// An alias to the type `t` if the boolean `cnd` is true, else undefined.
-template <bool cnd, typename t> using enable_if_t = typename enable_if<cnd, t>::type;
+template <bool cnd, typename t = void> using enable_if_t = typename enable_if<cnd, t>::type;
 
 } // namespace rex
