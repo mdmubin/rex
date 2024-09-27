201edb27ba72932af1393112fe80a8d06b8a5dbf
mdmubin <mdmubin.xyz@gmail.com>
2024-09-28 00:24:31 +0600

    fix `is_assignable` traits

--- rexbase/inc/rex/traits/is_assignable.hpp
+++ rexbase/inc/rex/traits/is_assignable.hpp
@@ -22,11 +22,11 @@ template <typename t, typename u> inline constexpr bool is_nothrow_assignable_v 
 //
 
 template <typename t> struct is_copy_assignable 
-    : bool_constant<is_assignable_v<t, add_lvalue_reference_t<const t>>> {};
+    : bool_constant<is_assignable_v<add_lvalue_reference_t<t>, add_lvalue_reference_t<const t>>> {};
 template <typename t> struct is_trivially_copy_assignable 
-    : bool_constant<is_trivially_assignable_v<t, add_lvalue_reference_t<const t>>> {};
+    : bool_constant<is_trivially_assignable_v<add_lvalue_reference_t<t>, add_lvalue_reference_t<const t>>> {};
 template <typename t> struct is_nothrow_copy_assignable 
-    : bool_constant<is_nothrow_assignable_v<t, add_lvalue_reference_t<const t>>> {};
+    : bool_constant<is_nothrow_assignable_v<add_lvalue_reference_t<t>, add_lvalue_reference_t<const t>>> {};
 
 /// @brief True if `t` may be copy assigned, else false. 
 template <typename t> inline constexpr bool is_copy_assignable_v = is_copy_assignable<t>::value;
@@ -38,11 +38,11 @@ template <typename t> inline constexpr bool is_nothrow_copy_assignable_v = is_no
 //
 
 template <typename t> struct is_move_assignable 
-    : bool_constant<is_assignable_v<t, add_rvalue_reference_t<t>>> {};
+    : bool_constant<is_assignable_v<add_lvalue_reference_t<t>, add_rvalue_reference_t<t>>> {};
 template <typename t> struct is_trivially_move_assignable 
-    : bool_constant<is_trivially_assignable_v<t, add_rvalue_reference_t<t>>> {};
+    : bool_constant<is_trivially_assignable_v<add_lvalue_reference_t<t>, add_rvalue_reference_t<t>>> {};
 template <typename t> struct is_nothrow_move_assignable 
-    : bool_constant<is_nothrow_assignable_v<t, add_rvalue_reference_t<t>>> {};
+    : bool_constant<is_nothrow_assignable_v<add_lvalue_reference_t<t>, add_rvalue_reference_t<t>>> {};
 
 /// @brief True if `t` may be move assigned, else false. 
 template <typename t> inline constexpr bool is_move_assignable_v = is_move_assignable<t>::value;
