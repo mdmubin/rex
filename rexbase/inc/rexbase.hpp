a6b2122faa40103fe45a007912f2c7c7e536e9fe
mdmubin <mdmubin.xyz@gmail.com>
2024-10-04 05:55:20 +0600

    Alphabetized and added `limits.hpp`

--- rexbase/inc/rexbase.hpp
+++ rexbase/inc/rexbase.hpp
@@ -2,7 +2,8 @@
 
 /* NOTE: this header file includes all the public header files of rexbase. */
 
-#include "rex/config.hpp"
-#include "rex/types.hpp"
 #include "rex/assert.hpp"
+#include "rex/config.hpp"
+#include "rex/limits.hpp"
 #include "rex/type_traits.hpp"
+#include "rex/types.hpp"
