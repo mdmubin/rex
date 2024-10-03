374325da364e4de9c0933ea0a6284a42fd139101
mdmubin <mdmubin.xyz@gmail.com>
2024-10-04 05:44:29 +0600

    Added `<limits>` implementation.

---
+++ rexbase/inc/rex/limit/float_denorm_style.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace rex
+{
+
+enum float_denorm_style
+{
+    denorm_indeterminate = -1,
+    denorm_absent        =  0,
+    denorm_present       =  1,
+};
+
+} // namespace rex
---
+++ rexbase/inc/rex/limit/float_round_style.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace rex
+{
+
+enum float_round_style
+{
+    round_indeterminate       = -1,
+    round_toward_zero         =  0,
+    round_to_nearest          =  1,
+    round_toward_infinity     =  2,
+    round_toward_neg_infinity =  3,
+};
+
+} // namespace rex
