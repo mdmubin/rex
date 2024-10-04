705b5fca082f6175673c5f86896c273af2ae02ca
mdmubin <mdmubin.xyz@gmail.com>
2024-10-04 11:54:08 +0600

    removed `rex::declval<t>` body as it was failing to compile on both clang & GCC in WSL.

--- rexbase/inc/rex/utils/declval.hpp
+++ rexbase/inc/rex/utils/declval.hpp
@@ -6,11 +6,7 @@ namespace rex
 {
 
 /// @brief Utility function meant to be used an unevaluated context to convert `t` to an rvalue reference type.
-/// @return May not be called, as doing so results in a `static_assert` fail.
 template <typename t>
-add_rvalue_reference_t<t> declval() noexcept
-{
-    static_assert(false, "rex::declval may not be evaluated.");
-}
+add_rvalue_reference_t<t> declval() noexcept;
 
 } // namespace rex
