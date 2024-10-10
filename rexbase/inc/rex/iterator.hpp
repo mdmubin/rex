be9166b2be7dd199afc3659f7daddad74499be23
mdmubin <mdmubin.xyz@gmail.com>
2024-10-10 14:02:21 +0600

    Added `rex/iterator.hpp` with includes of relevant headers.

---
+++ rexbase/inc/rex/iterator.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "rex/iter/advance.hpp"
+#include "rex/iter/back_insert_iterator.hpp"
+#include "rex/iter/container_helpers.hpp"
+#include "rex/iter/distance.hpp"
+#include "rex/iter/front_insert_iterator.hpp"
+#include "rex/iter/insert_iterator.hpp"
+#include "rex/iter/iterator_tags.hpp"
+#include "rex/iter/iterator_traits.hpp"
+#include "rex/iter/iterator.hpp"
+#include "rex/iter/move_iterator.hpp"
+#include "rex/iter/next.hpp"
+#include "rex/iter/prev.hpp"
+#include "rex/iter/reverse_iterator.hpp"
+
+/* NOTE: Stream iterators are as of yet not implemented. Maybe I will implement them later on. */
