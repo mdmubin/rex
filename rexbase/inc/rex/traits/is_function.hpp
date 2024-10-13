1e70c6e281483247bbb415f3470a0ff27257e792
mdmubin <mdmubin.xyz@gmail.com>
2024-10-13 14:49:20 +0600

    Update `is_function` to suppress MSVC warning for const function.

--- rexbase/inc/rex/traits/is_function.hpp
+++ rexbase/inc/rex/traits/is_function.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
+#include "rex/config.hpp"
 #include "rex/traits/integral_constant.hpp"
 #include "rex/traits/is_const_volatile.hpp"
 #include "rex/traits/is_reference.hpp"
+
+#if defined(REX_COMPILER_MSVC) || defined(REX_COMPILER_CLANG_CL)
+    #pragma warning(push)
+    #pragma warning(disable : 4180) // adding const to function warning.
+#endif
 
 namespace rex {
 
@@ -13,3 +19,7 @@ template <typename t> struct is_function
 template <typename t> inline constexpr bool is_function_v = is_function<t>::value;
 
 } // namespace rex
+
+#if defined(REX_COMPILER_MSVC) || defined(REX_COMPILER_CLANG_CL)
+    #pragma warning(pop)
+#endif
