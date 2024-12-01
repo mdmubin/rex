3bcae8f3554c01386285e53861eabb069c1b931f
mdmubin <mdmubin.xyz@gmail.com>
2024-12-01 19:17:00 +0600

    Added store operations for atomic storage types.

--- rexbase/inc/rex/atomic/atomic_operations.hpp
+++ rexbase/inc/rex/atomic/atomic_operations.hpp
@@ -10,18 +10,24 @@
 
 #if defined(REX_COMPILER_CLANG_CL) || defined(REX_COMPILER_MSVC)
     #include <intrin.h>
-    #pragma intrinsic(_ReadWriteBarrier)
+    #pragma intrinsic(_InterlockedExchange)
+    #pragma intrinsic(_InterlockedExchange8)
+    #pragma intrinsic(_InterlockedExchange16)
+    #pragma intrinsic(_InterlockedExchange64)
     #pragma intrinsic(_InterlockedCompareExchange128)
+    #pragma intrinsic(_ReadWriteBarrier)
     #define REX_COMPILER_BARRIER() _ReadWriteBarrier() // deprecated. but what to use instead?
 #else
-    #define REX_COMPILER_BARRIER() asm volatile("" ::: "memory")
+    #define REX_COMPILER_BARRIER() __asm__ __volatile__("" ::: "memory")
 #endif
 
 // clang-format off
 
 #define REX_VERIFY_ATOMIC_LOAD_ORDER(order) REX_VERIFY_MSG( \
-        (order <= rex::memory_order_seq_cst && order != rex::memory_order_release && order != rex::memory_order_acq_rel),\
-        "Invalid memory order constraint for atomic load."\
+        (order <= rex::memory_order_seq_cst &&              \
+         order != rex::memory_order_release &&              \
+         order != rex::memory_order_acq_rel),               \
+        "Invalid memory order constraint for atomic load."  \
     )
 #define REX_VERIFY_ATOMIC_STORE_ORDER(order) REX_VERIFY_MSG( \
     (order <= rex::memory_order_seq_cst &&                   \
@@ -40,8 +46,6 @@
         } \
     } while (0)
 
-// clang-format on
-
 //
 
 namespace rex::impl
@@ -52,7 +56,7 @@ struct atomic_operations
 { // clang-format off
     static_assert(
         sizeof(t) == 1 || sizeof(t) == 2 || sizeof(t) == 4 || sizeof(t) == 8 || sizeof(t) == 16,
-        "Lockfree atomic operations are only supported for data that has the width of an integer. "
+        "Lockfree atomic operations are only supported for data that has the width of an integral type. "
         "(i.e. sizeof(t) is 1, 2, 4, 8 or 16 bytes)"
     );
 }; // clang-format on
@@ -67,6 +71,33 @@ struct atomic_operations<t, 1>
         REX_PLACE_ATOMIC_LOAD_BARRIER(order);
         return reinterpret_cast<t &>(value);
     }
+
+    static void store(t &storage, t value, memory_order order)
+    {
+        REX_VERIFY_ATOMIC_STORE_ORDER(order);
+        switch (order)
+        {
+        case memory_order_relaxed:
+            *addressof(reinterpret_cast<volatile u8 &>(storage)) = reinterpret_cast<u8 &>(value);
+            break;
+        case memory_order_release:
+            REX_COMPILER_BARRIER();
+            *addressof(reinterpret_cast<volatile u8 &>(storage)) = reinterpret_cast<u8 &>(value);
+            break;
+        case memory_order_seq_cst:
+#if defined(REX_COMPILER_MSVC)
+            _InterlockedExchange8(addressof(reinterpret_cast<volatile char &>(storage)),
+                                  reinterpret_cast<char &>(value));
+            break;
+#else
+            REX_COMPILER_BARRIER();
+            __asm__ __volatile__("xchg %0, %1" : "+q"(value) : "m"(storage));
+            break;
+#endif
+        default:
+            REX_UNREACHABLE();
+        }
+    }
 };
 
 template <typename t>
@@ -78,6 +109,33 @@ struct atomic_operations<t, 2>
         u16 value = *addressof(reinterpret_cast<volatile u16 &>(storage));
         REX_PLACE_ATOMIC_LOAD_BARRIER(order);
         return reinterpret_cast<t &>(value);
+    }
+
+    static void store(t &storage, t value, memory_order order)
+    {
+        REX_VERIFY_ATOMIC_STORE_ORDER(order);
+        switch (order)
+        {
+        case memory_order_relaxed:
+            *addressof(reinterpret_cast<volatile u16 &>(storage)) = reinterpret_cast<u16 &>(value);
+            break;
+        case memory_order_release:
+            REX_COMPILER_BARRIER();
+            *addressof(reinterpret_cast<volatile u16 &>(storage)) = reinterpret_cast<u16 &>(value);
+            break;
+        case memory_order_seq_cst:
+#if defined(REX_COMPILER_MSVC)
+            _InterlockedExchange16(addressof(reinterpret_cast<volatile short &>(storage)),
+                                   reinterpret_cast<short &>(value));
+            break;
+#else
+            REX_COMPILER_BARRIER();
+            __asm__ __volatile__("xchg %0, %1" : "+r"(value) : "m"(storage));
+            break;
+#endif
+        default:
+            REX_UNREACHABLE();
+        }
     }
 };
 
@@ -91,6 +149,33 @@ struct atomic_operations<t, 4>
         REX_PLACE_ATOMIC_LOAD_BARRIER(order);
         return reinterpret_cast<t &>(value);
     }
+
+    static void store(t &storage, t value, memory_order order)
+    {
+        REX_VERIFY_ATOMIC_STORE_ORDER(order);
+        switch (order)
+        {
+        case memory_order_relaxed:
+            *addressof(reinterpret_cast<volatile u32 &>(storage)) = reinterpret_cast<u32 &>(value);
+            break;
+        case memory_order_release:
+            REX_COMPILER_BARRIER();
+            *addressof(reinterpret_cast<volatile u32 &>(storage)) = reinterpret_cast<u32 &>(value);
+            break;
+        case memory_order_seq_cst:
+#if defined(REX_COMPILER_MSVC)
+            _InterlockedExchange(addressof(reinterpret_cast<volatile long &>(storage)),
+                                 reinterpret_cast<long &>(value));
+            break;
+#else
+            REX_COMPILER_BARRIER();
+            __asm__ __volatile__("xchg %0, %1" : "+r"(value) : "m"(storage));
+            break;
+#endif
+        default:
+            REX_UNREACHABLE();
+        }
+    }
 };
 
 template <typename t>
@@ -103,23 +188,82 @@ struct atomic_operations<t, 8>
         REX_PLACE_ATOMIC_LOAD_BARRIER(order);
         return reinterpret_cast<t &>(value);
     }
+
+    static void store(t &storage, t value, memory_order order)
+    {
+        REX_VERIFY_ATOMIC_STORE_ORDER(order);
+        switch (order)
+        {
+        case memory_order_relaxed:
+            *addressof(reinterpret_cast<volatile u64 &>(storage)) = reinterpret_cast<u64 &>(value);
+            break;
+        case memory_order_release:
+            REX_COMPILER_BARRIER();
+            *addressof(reinterpret_cast<volatile u64 &>(storage)) = reinterpret_cast<u64 &>(value);
+            break;
+        case memory_order_seq_cst:
+#if defined(REX_COMPILER_MSVC)
+            _InterlockedExchange64(addressof(reinterpret_cast<volatile __int64 &>(storage)),
+                                   reinterpret_cast<__int64 &>(value));
+            break;
+#else
+            REX_COMPILER_BARRIER();
+            __asm__ __volatile__("xchg %0, %1" : "+r"(value) : "m"(storage));
+            break;
+#endif
+        default:
+            REX_UNREACHABLE();
+        }
+    }
 };
 
 template <typename t>
 struct atomic_operations<t, 16>
 {
-    /// @note On clang/gcc, it is important that `storage` be aligned to 16-bytes.
     static t load(t &storage, memory_order order)
     {
 #if defined(REX_COMPILER_MSVC)
         i64 value[2];
-        _InterlockedCompareExchange128((volatile long long *)addressof(storage), 0, 0, value);
+        (void)_InterlockedCompareExchange128((volatile long long *)addressof(storage), 0, 0, value);
         return *reinterpret_cast<t *>(value);
 #else
-        __extension__ __int128 value;
-        asm volatile("movdqa %0, %1" : "=x"(value) : "m"(storage));
+        __int128 value;
+        __asm__ __volatile__("movdqa %0, %1" : "=x"(value) : "m"(storage));
         REX_PLACE_ATOMIC_LOAD_BARRIER(order);
         return reinterpret_cast<t &>(value);
+#endif
+    }
+
+    static void store(t &storage, t value, memory_order order)
+    {
+        REX_VERIFY_ATOMIC_STORE_ORDER(order);
+#if defined(_MSC_VER)
+        auto cpy = storage;
+        i64 *cmp = reinterpret_cast<i64 *>(addressof(cpy));
+        i64 *val = reinterpret_cast<i64 *>(addressof(value));
+        i64 *dst = reinterpret_cast<i64 *>(addressof(storage));
+        (void)_InterlockedCompareExchange128(dst, val[1], val[0], cmp);
+#else
+        auto cpy = storage; // create a copy instead of writing directly into storage.
+        i64 *cmp = reinterpret_cast<i64 *>(addressof(cpy));
+        i64 *val = reinterpret_cast<i64 *>(addressof(value));
+
+        switch (order)
+        {
+        case memory_order_relaxed:
+            break;
+        case memory_order_release:
+        case memory_order_seq_cst:
+            REX_COMPILER_BARRIER();
+            break;
+        default:
+            REX_UNREACHABLE();
+        }
+
+        __asm__ __volatile__("lock cmpxchg16b %0"
+                             : "=m"(storage)
+                             : "a"(cmp[0]), "d"(cmp[1]), "b"(val[0]), "c"(val[1])
+                             : "cc", "memory"); // do I really need the cc clobber here?
 #endif
     }
 };
