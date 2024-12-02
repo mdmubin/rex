6da76f9c9159ad0ffe7d6cf0d2c4a0c982774ec2
Mubin <mdmubin.xyz@gmail.com>
2024-12-02 19:45:18 +0600

    Added `atomic.hpp` includes to `CMakeLists.txt` and `rexbase.hpp`.

--- rexbase/inc/rexbase.hpp
+++ rexbase/inc/rexbase.hpp
@@ -3,7 +3,9 @@
 /* NOTE: this header file includes all the public header files of rexbase. */
 
 #include "rex/assert.hpp"
+#include "rex/atomic.hpp"
 #include "rex/config.hpp"
+#include "rex/iterator.hpp"
 #include "rex/limits.hpp"
 #include "rex/type_traits.hpp"
 #include "rex/types.hpp"
